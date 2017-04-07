fcb2c3a5cf7659fec0ccfb6621ae0a8426a6c55a
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-04-06 17:41:31 -0700

    Add pseudocode

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+
+
+// Strict button
+
+// Start
+
+// Do start dance
+
+// Generate computer array
+
+// Iterate count
+
+// Play computer array to count -1
+
+// Check if player input matches computer array
+
+// if no match:
+
+// If strict, restart
+
+// if not strict replay computer array
+
+// if match
+
+// Check for win, do win dance, do start dance again
+
+// Play computer array to count -1
+
+// Reset button
